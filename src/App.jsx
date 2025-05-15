297c5532b8a2e7e66783bfc2a853a57c04852789
Nikoloz Tevdoradze <nikoloztevdoradze08@gmail.com>
2025-05-15 04:08:53 +0400

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ function App() {
   return (
     <>
 
-      <div className="body flex justify-center bg-[#171823] min-h-[100vh] pt-[50px]! bg-[url('/images/bg-desktop-dark.jpg')] bg-no-repeat bg-[length:100%_auto] max-xl:bg-[url('/images/bg-mobile-dark.jpg')]" style={darkLight ? { backgroundColor: "#FAFAFA", backgroundImage: "url('/images/bg-desktop-light.jpg')" } : null}>
+      <div className="zbody flex justify-center bg-[#171823] min-h-[100vh] pt-[50px]! bg-[url('/images/bg-desktop-dark.jpg')] bg-no-repeat bg-[length:100%_auto] max-xl:bg-[url('/images/bg-mobile-dark.jpg')]" style={darkLight ? { backgroundColor: "#FAFAFA", backgroundImage: "url('/images/bg-desktop-light.jpg')" } : null}>
         <div className='w-[540px] max-md:w-[100%] pl-[32px]! pr-[32px]! flex flex-col gap-[20px]'>
           <Text setDarkLight={setDarkLight} darkLight={darkLight} />
           <Input handleClick={handleClick} input={input} setInput={setInput} darkLight={darkLight} />
